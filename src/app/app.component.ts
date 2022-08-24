182733696af6b07e61118db0aac61f7209a750fa
Marianela <masham1860@gmail.com>
2022-08-24 18:15:00 -0300

    Actualizando css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  titulo = 'Registro de usuarios';
+  mensaje= '';
+  nombre:string='';
+  apellido:string='';
+  cargo:string='';
+  area:string='';
+  registrado= false;
+  entradas: any[];
+
+  constructor(){
+    this.entradas=[
+      {titulo:'Pedro Picapiedra, cargo: administrativo, área: contable '},
+      {titulo:'Laura Lopez, cargo: recepcionista, área: atención veterinaria'},
+      {titulo:'Ezequiel Sanchez, cargo:  docente, área: primaria'},
+      {titulo:'Maria Mendoza, cargo: diseñadora, área: creativa'},
+    ]
+
+  }
+
+
+
+  registrarUsuario(){
+
+
+    this.registrado= true
+    this.mensaje='¡' + this.nombre+' '+ this.apellido + ' se ha registrado con éxito !';
+  }
 }
